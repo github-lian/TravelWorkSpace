f4d9d5f3a57135bb27d6068948a50e13f7633f56
github-lian <1364332674@qq.com>
2018-10-24 20:48:17 +0800

    change

---
+++ app/src/main/java/com/example/lian/travel/Util/PermisionUtils.java
@@ -0,0 +1,43 @@
+package com.example.lian.travel.Util;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * Created by lian on 2018/4/24.
+ */
+public class PermisionUtils {
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_NOTIFICATION_POLICY,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.RECEIVE_SMS,
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * If the app does not has permission then the user will be prompted to
+     * grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }
+    }
+}
