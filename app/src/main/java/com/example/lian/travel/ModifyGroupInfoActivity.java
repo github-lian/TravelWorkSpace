8bc241637eaefa1729b6ce6e916f3558e551fa26
chen <2272520273@qq.com>
2018-11-01 09:47:48 +0800

    update

---
+++ app/src/main/java/com/example/lian/travel/ModifyGroupInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.lian.travel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModifyGroupInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_group_info);
+    }
+}
